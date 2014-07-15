b4aae414a5f062df1971c052c1f9743cf6345de3
woodall <dave@woodalls.me>
2014-07-15 06:10:49 -0600

    chapter 7 with typo

--- best_quotes/sqlite_test.rb
+++ best_quotes/sqlite_test.rb
@@ -3,3 +3,16 @@ require 'rulers/sqlite_model'
 
 class MyTable < Rulers::Model::SQLite; end
 STDERR.puts MyTable.schema.inspect
+
+mt = MyTable.create "title" => "It happened!", 
+  "posted" => 1, "body" => "It did work, thank the Lord!"
+mt = MyTable.create "title" => "I saw it!"
+
+puts "Count: #{MyTable.count}"
+
+top_id = mt["id"].to_i
+
+(1..top_id).each do |id|
+  mt_id = MyTable.find(id)
+  puts "Found title #{mt_id['title']}."
+end
